123c1c41437ec06f70ccc813c86e93ec1967d92c
Jess Skelton <jessskelton888@gmail.com>
2024-03-13 23:36:43 -0500

    edited file structure -- Updated package.json, vite.config, index.html, main.jsx, app.jsx -- Added components and asset folders -- Added images for possible later use to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+// Bringing in the appropriate React imports
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// Bringing in the pages the router will use to conditionally show the appropriate views
 import App from './App.jsx'
 import './index.css'
 
+// Accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: < addlater/>,
+    children:[
+      {
+
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
