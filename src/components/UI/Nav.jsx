2a51bc7cda556378761bfa490ecaf5311b273d41
Jess Skelton <jessskelton888@gmail.com>
2024-03-14 00:21:13 -0500

    Took styling that was from vite build out on index.css and app.css --- Updated & completed main.jsx & app.jsx & index.html --- Added ErrorPage.jsx, HomePage.jsx, PortfolioPage.jsx, ResumePage.jsx, and ContactPage.jsx --- Added and completed Nav.jsx to components folder

---
+++ src/components/UI/Nav.jsx
@@ -0,0 +1,42 @@
+// React imports
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+// Navbar -----------------style later
+const Nav = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                {/* add logo below later? */}
+                <div className="brand">Logo</div>
+                <div className={`menu-toggle ${isOpen ? 'active' : ''}`} onClick={toggleMenu}>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                    <div className="bar"></div>
+                </div>
+                <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Me</Link>
+                    </li>
+                    <li>
+                        <Link to="/resume">Resume</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,36 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 // Bringing in the pages the router will use to conditionally show the appropriate views
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
+import ErrorPage from './pages/ErrorPage.jsx';
+import HomePage from './pages/HomePage.jsx';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
 
 // Accessible routes, and which components respond to which URL
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: < addlater/>,
+    errorElement: <ErrorPage />,
     children:[
       {
-
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
       },
     ],
   },
